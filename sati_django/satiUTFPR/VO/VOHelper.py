160057fd8120c646ca001ae025670c3bf57b71b2
Vinicius Custodio <vinicustod@hotmail.com>
2016-08-18 20:39:47 -0300

    singup form working
    signup

--- sati_django/satiUTFPR/VO/VOHelper.py
+++ sati_django/satiUTFPR/VO/VOHelper.py
@@ -1,8 +1,12 @@
+from aenum import Enum
 
 
-
-class Acess(enum):
+class Acess(Enum):
     Administrator = 0
     Instructor = 1
     Student = 2
 
+
+class Status(Enum):
+    Inativo = 0
+    Ativo = 1
--- sati_django/satiUTFPR/views.py
+++ sati_django/satiUTFPR/views.py
@@ -1,17 +1,11 @@
+# import os, sys
 from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from controller import LoginController
-
-# Create your views here.
-
-# class LunchForm(forms.Form):
-#     submitter = forms.CharField(label='Your name')
-#     food = forms.CharField(label='What did you eat?')
-#
-#
-# lunch_form = LunchForm(auto_id=False)
-
+from models import Person
+from VO.VOHelper import Acess
+from VO.VOHelper import Status
 
 def index(request):
     # lunches = Lunch.objects.all()
@@ -27,12 +21,38 @@ def login(request):
         'sati_utfpr/login.html'
     )
 
+
 def test(request):
     return render(
         request,
         'sati_utfpr/test.html'
     )
 
+
+def signup(request):
+    return render(
+        request,
+        'sati_utfpr/signup.html'
+    )
+
+
 def newLogin(request):
     loginController = LoginController(request)
     return loginController.newLogin()
+
+
+def registry_person(request):
+    person = Person()
+    person.name = request.POST["nome_completo"]
+    person.dateBirth = request.POST["data_nascimento"]
+    person.institution = request.POST["instituicao"]
+    person.password = request.POST["senha"]
+    person.cpf = request.POST["cpf"]
+    person.academicRegistry = request.POST["registro_academico"]
+    person.role = 0
+    person.status = 1
+    person.save()
+    return render(
+        request,
+        'sati_utfpr/test.html'
+    )
