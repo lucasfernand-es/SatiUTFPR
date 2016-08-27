3eea40271e14e8593564f8369d82c93bbe68d68d
Vinicius Custodio <vinicustod@hotmail.com>
2016-08-27 13:59:41 -0300

    Changed to Django 1.10
    Changed settings and urlpatterns (use to have old syntax)

---
+++ sati_django/sati/controller/PersonController.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from models import Person
+
+
+def registry_person(request):
+    person = Person()
+    person.name = request.POST["nome_completo"]
+    person.dateBirth = request.POST["data_nascimento"]
+    person.institution = request.POST["instituicao"]
+    person.password = request.POST["senha"]
+    person.cpf = request.POST["cpf"]
+    person.email = request.POST["email"]
+    person.academicRegistry = request.POST["registro_academico"]
+    person.role = 0
+    person.status = 1
+    person.save()
+    return render(
+        request,
+        'sati_utfpr/test.html'
+    )
