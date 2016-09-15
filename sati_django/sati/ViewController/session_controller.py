577cfa69e5bc959cfa4ca9daefe6e5bdd00babca
Vinicius Custodio <vinicustod@hotmail.com>
2016-09-15 20:41:31 -0300

    Session Spots Errors

--- sati_django/sati/ViewController/session_controller.py
+++ sati_django/sati/ViewController/session_controller.py
@@ -3,23 +3,28 @@ from sati.models import Session, Participant
 
 
 def get_session_spots(request):
-    session = Session.objects.filter(id=request.POST.get('event_id'))
+    session = Session.objects.filter(id=request.POST.get('session_id'))
     context = {
         'session_spots' : session.spots
     }
     return context
 
 def get_session_available_spots(request):
-    participants = Participant.objects.filter(is_confirmed=True, event_id=request.POST.get('event_id'))
+    participants = Participant.objects.filter(is_confirmed=True, event_id=request.POST.get('session_id'))
     session = Session.objects.filter(id=request.POST.get('event_id'))
-    session_available_spots = session.spots - len(participants)
-    if session_available_spots > 0:
-        context ={
-            'session_available_spots': session_available_spots
-        }
+    context = {}
+
+    if session:
+        session_available_spots = session.spots - len(participants)
     else:
-        context = {
-            'error' : 'not_spots_available'
-        }
+        context['error'] = True
+        context['error_msg'] = 'session_not_found'
+        
+    if session_available_spots > 0:
+        context['session_available_spots'] = session_available_spots
+        context['error'] = False
+    else:
+        context['error_msg'] = 'not_spots_available'
+        context['error']= True
 
     return context
