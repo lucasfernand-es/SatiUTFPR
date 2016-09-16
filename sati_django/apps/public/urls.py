893ffb9745d1280a537f98b2f653f012ead1c081
Vinicius Custodio <vinicustod@hotmail.com>
2016-09-15 23:18:29 -0300

    Session methods

--- sati_django/apps/public/urls.py
+++ sati_django/apps/public/urls.py
@@ -15,8 +15,14 @@ urlpatterns = [
     # url(r'^events/card_detail', views.event_card_detail, name='event-card-detail'),
 
     url(r'^event/(?P<event_id>[0-9]+)/$', views.event_detail, name='event-detail'),
-    url(r'^event/(?P<event_id>[0-9]+)/spots_event/$', session_controller.get_events_spots , name='event-spots'),
-    url(r'^event/(?P<event_id>[0-9]+)/(?P<session_id>[0-9]+)/spots_session/$', session_controller.get_session_spots, name='session-spots'),
+
+    url(r'^event/(?P<event_id>[0-9]+)/spots_event/$', session_controller.get_events_spots,
+        name='event-spots'),
+    url(r'^event/(?P<event_id>[0-9]+)/spots_event_available/$', session_controller.get_events_spots_available,
+        name='event-spots-available'),
+
+    url(r'^event/(?P<event_id>[0-9]+)/(?P<session_id>[0-9]+)/spots_session/$', session_controller.get_session_spots,
+        name='session-spots'),
     url(r'^event/(?P<event_id>[0-9]+)/(?P<session_id>[0-9]+)/spots_session_available/$',
         session_controller.get_session_available_spots, name='session-spots-available')
 
--- sati_django/sati/ViewController/session_controller.py
+++ sati_django/sati/ViewController/session_controller.py
@@ -1,24 +1,48 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, HttpResponse
 from sati.models import Session, Participant, Event
 
 
-def get_events_spots(request):
-    sessions = Session.objects.filter(event_id=request.POST.get('event_id'))
+def get_events_spots(request, event_id):
+    sessions = Session.objects.filter(event_id=request.POST.get(event_id))
     context = {}
 
     if sessions:
+        spots_event = 0
         for session in sessions:
-            spots_event = session.spots + spots_event
+            spots_event += session.spots
         context['event_spots'] = spots_event
         context['error'] = False
     else:
         context['error'] = True
         context['error_msg'] = 'session_not_found'
-    return context
+    return HttpResponse(context)
 
 
-def get_session_spots(request):
-    session = Session.objects.filter(id=request.POST.get('session_id'))
+def get_events_spots_available(request, event_id):
+    sessions = Session.objects.filter(event_id=request.POST.get(event_id))
+    context = {}
+
+    if sessions:
+        event_available_spots = 0
+        for session in sessions:
+            participants = Participant(is_confirmed=True, session_id=session.id)
+            session_available_spots = session.spots - len(participants)
+            event_available_spots += session_available_spots
+
+        if event_available_spots > 0:
+            context['event_available_spots'] = event_available_spots
+            context['error'] = False
+        else:
+            context['error_msg'] = 'no_spots_available'
+            context['error'] = True
+    else:
+        context['error'] = True
+        context['error_msg'] = 'session_not_found'
+    return HttpResponse(context)
+
+
+def get_session_spots(request, event_id, session_id):
+    session = Session.objects.filter(id=request.POST.get(session_id))
     context = {}
 
     if session:
@@ -28,11 +52,12 @@ def get_session_spots(request):
         context['error'] = True
         context['error_msg'] = 'session_not_found'
 
-    return context
+    return HttpResponse(context)
 
-def get_session_available_spots(request):
-    participants = Participant.objects.filter(is_confirmed=True, session_id=request.POST.get('session_id'))
-    session = Session.objects.filter(id=request.POST.get('session_id'))
+
+def get_session_available_spots(request, event_id,session_id):
+    participants = Participant.objects.filter(is_confirmed=True, session_id=request.POST.get(session_id))
+    session = Session.objects.filter(id=request.POST.get(session_id))
     context = {}
 
     if session:
@@ -47,6 +72,6 @@ def get_session_available_spots(request):
         context['error'] = True
         context['error_msg'] = 'session_not_found'
         
+    return HttpResponse(context)
 
 
-    return context
