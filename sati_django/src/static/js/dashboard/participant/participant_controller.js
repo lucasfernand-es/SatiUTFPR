dec4bfe674a6428465c045e0a31d3a0bea18c15a
Lucas Fernandes <lucas.emanuel19@gmail.com>
2016-09-19 14:45:53 -0300

    Sign up fix

--- sati_django/src/static/js/dashboard/participant/participant_controller.js
+++ sati_django/src/static/js/dashboard/participant/participant_controller.js
@@ -11,6 +11,43 @@
                                                                EventLabel) {
         var participantCtrl = this;
 
+        var promise;
+
+
+        participantCtrl.loadParticipants = function () {
+
+        };
+
+    });
+
+
+app.controller('EventDetailCtrl', function EventDetailCtrl($scope, $log, $http,
+                                                               ModelUtils, Urls, Toast, Label,
+                                                               EventLabel) {
+        var eventDetail = this;
+        eventDetail.event_id = $scope.event_id;
+
+        var promise;
+
+        // Factories
+        $scope.EventLabel = EventLabel;
+        $scope.Label = Label;
+
+        $scope.message = '';
+
+        eventDetail.loadEvent = function () {
+
+            promise = ModelUtils.get_request(Urls.get_event(eventDetail.event_id))
+                .then(function (response) {
+                    eventDetail.event = response;
+
+                }, function (response) {
+                    Toast.showToast(response.status + ' ' + response.statusText);
+                });
+        };
+
+        eventDetail.loadEvent();
+
     });
 
 
